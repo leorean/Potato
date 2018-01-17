03c87999eb0212fe99ae970314d9c779ff0c25ba
Peter S <peter@anyline.io>
2018-01-18 00:25:14 +0100

    added tilesets

--- Potato/Assets/Scripts/PlayerController.cs
+++ Potato/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,11 @@ public class PlayerController : MonoBehaviour {
     public string leftKey = "left";
     public string rightKey = "right";
 
-    public float jumpSpeed = 2f;
-    public float rotationSpeed = 8f;
+    public float jumpSpeed = 4f;
+    public float rotationSpeed = 3.8f;
     public float horizontalSpeed = 2f;
+    public float maxVelocityX = 4f;
+    public float maxVelocityY = 5f;
 
     public enum Direction
     {
@@ -18,57 +20,94 @@ public class PlayerController : MonoBehaviour {
         Right = 1
     }
     public Direction direction = Direction.Right;
-
+    
     bool isJumpPressed;
     bool isLeftPressing;
     bool isRightPressing;
-
     bool isAlreadyJumped;
 
     Rigidbody2D rb2d;
-
     float scaleX;
     float scaleY;
-
-	// Use this for initialization
-	void Start () {
+    float angularDrag;
+    
+    // Use this for initialization
+    void Start () {
 
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         scaleX = transform.localScale.x;
         scaleY = transform.localScale.y;
+        angularDrag = rb2d.angularDrag;
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        isJumpPressed = Input.GetKeyDown(jumpKey);
+    // Update is called once per frame
+    void Update () {
+
+        isJumpPressed = Input.GetKey(jumpKey);
         isLeftPressing = Input.GetKey(leftKey);
         isRightPressing = Input.GetKey(rightKey);
 
+        // flip player when facing other direction
         transform.localScale = new Vector3(scaleX * (int)direction, scaleY);
     }
-
-
+    
     void FixedUpdate()
-    {        
-        if (isJumpPressed)
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
-            rb2d.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+    {
+        rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * Mathf.Min(Mathf.Abs(rb2d.velocity.x), maxVelocityX),
+            Mathf.Sign(rb2d.velocity.y) * Mathf.Min(Mathf.Abs(rb2d.velocity.y), maxVelocityY));
 
+        if (IsOnGround())
+            isAlreadyJumped = false;
+
+        if (IsOnGround())
+        {
+            rb2d.angularDrag = angularDrag;
+        } else
+        {
+            rb2d.angularDrag = 0;
         }
 
+        if (isJumpPressed)
+        {
+            if (!isAlreadyJumped)
+            {
+                isAlreadyJumped = true;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                rb2d.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            }
+        }
+        
         if (isLeftPressing)
         {
-            rb2d.MoveRotation(rb2d.rotation + rotationSpeed);
-            rb2d.AddForce(Vector2.left * horizontalSpeed);
-            direction = Direction.Left;
+            //rb2d.AddTorque(rotationSpeed);
+            rb2d.MoveRotation(rb2d.rotation + rotationSpeed);            
+            if (rb2d.velocity.x < 0.1f) direction = Direction.Left;
+
+            if (!IsOnGround())
+            {
+                rb2d.AddForce(Vector2.left * horizontalSpeed);
+            }
         }
         if (isRightPressing)
         {
-            rb2d.MoveRotation(rb2d.rotation - rotationSpeed);
-            rb2d.AddForce(Vector2.right * horizontalSpeed);
-            direction = Direction.Right;
+            //rb2d.AddTorque(-rotationSpeed);
+            rb2d.MoveRotation(rb2d.rotation - rotationSpeed);            
+            if (rb2d.velocity.x > 0.1f) direction = Direction.Right;
+            if (!IsOnGround())
+            {
+                rb2d.AddForce(Vector2.right * horizontalSpeed);
+            }
         }
+    }
+
+    //////
+
+    bool IsOnGround()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, LayerMask.GetMask("BlockLayer"));
+        /*if (hit.collider != null)
+            print("HIT: " + hit.collider.name);*/
+        return (hit.collider != null);
     }
 }
